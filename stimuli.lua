fb1769ff74033301276898138cffe1b78a67c069
Tom Piercy <tap32@medschl.cam.ac.uk>
2016-04-14 11:45:14 +0100

    Added function get wildcard stimuli

--- stimuli.lua
+++ stimuli.lua
@@ -9,11 +9,19 @@ local img={
   "img/stimuli/1.png",
   "img/stimuli/2.png",
   "img/stimuli/3.png",
-  "img/stimuli/4.png"
+}
+
+local wildImg={
+  [3]="img/stimuli/wildcard3.png",
+  [6]="img/stimuli/wildcard6.png",
 }
 
 function getStimulus(n)
   return display.newImage(img[n])
+end
+
+function getWildcardSimuli(presses)
+  return display.newImage(wildImg[presses])
 end
 
 return M
