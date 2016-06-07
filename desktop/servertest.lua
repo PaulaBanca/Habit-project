25979831e42769617e3a7d984cea5bd5fd220c5b
Tom Piercy <tap32@medschl.cam.ac.uk>
2016-06-07 18:04:19 +0100

    Allow arrow keys to be used for Makey Makey support

--- desktop/servertest.lua
+++ desktop/servertest.lua
@@ -6,13 +6,22 @@ local NUM_KEYS=NUM_KEYS
 local Runtime=Runtime
 local tonumber=tonumber
 local print=print
+local type=type
 
 setfenv(1,M)
+
+local arrowMapping={
+  up=1,
+  right=2,
+  down=3,
+  left=4
+}
 
 local down={}
 Runtime:addEventListener("key", function(event)
   local key=tonumber(event.keyName)
-  if not key or key<1 or key>NUM_KEYS then
+  key=key or arrowMapping[event.keyName]
+  if not key or type (key)=="number" and key<1 or key>NUM_KEYS then
     return false
   end
   if event.phase=="down" and not down[key] then
