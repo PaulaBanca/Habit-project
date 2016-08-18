2b886827b166e6a129bb2ac8d54c3598aa24d302
Tom Piercy <tap32@medschl.cam.ac.uk>
2016-08-18 14:56:25 +0100

    Change VI intervals

--- desktop/scenes/practiceintro.lua
+++ desktop/scenes/practiceintro.lua
@@ -128,8 +128,8 @@ local pageSetup={
     {text="In this last task, you also need to choose between 2 chests. You open chests by playing the sequence that matches the symbol on it.\n\nSome symbols or chests will reward you more often than the others.\n\nRemember you are playing for real money! You will receive your winnings by the end of the study.\n\nTry to win as much as you can!",
     onKeyPress=function()
       doorschedule.start()
-      vischedule.setup(1,35000,1000)
-      vischedule.setup(2,35000,1000)
+      vischedule.setup(1,20000,1000)
+      vischedule.setup(2,20000,1000)
       vischedule.setup(3,4000,1000)
       vischedule.start()
       function run()
