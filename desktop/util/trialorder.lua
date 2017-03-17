b19f6beb7ce842436d541d47f379f3082fd18355
Tom Piercy <tap32@medschl.cam.ac.uk>
2017-03-17 16:13:17 +0000

    Fix trial order for small numbers

--- desktop/util/trialorder.lua
+++ desktop/util/trialorder.lua
@@ -15,26 +15,36 @@ function generate(options, bucketSize)
   assert(total%bucketSize==0)
 
   local numBuckets=total/bucketSize
-
   for i=1, #options do
-    options[i].numPerBucket=options[i].n/numBuckets
+    options[i].numPerBucket=(options[i].n)/numBuckets
     options[i].accumulator=0
   end
-  
-  local buckets={}
-  for i=1, numBuckets do
-    local bucket={}
-    for k=1,#options do
-      local opt=options[k]
-      local num=math.floor(opt.numPerBucket+opt.accumulator)
 
-      opt.accumulator=opt.numPerBucket-num
-      for j=#bucket+1,#bucket+num do
-        bucket[j]=opt.value
+  local function makeBucket()
+    local bucket={}
+    while true do
+      for k=1,#options do
+        local opt=options[k]
+        local num=math.floor(opt.numPerBucket+opt.accumulator)
+        if num+#bucket>bucketSize then
+          num=bucketSize-#bucket
+        end
+        opt.accumulator=opt.numPerBucket-num
+        for j=#bucket+1,#bucket+num do
+          bucket[j]=opt.value
+        end
+        if #bucket==bucketSize then
+          return bucket
+        end
       end
     end
-    assert(#bucket==bucketSize,#bucket .. " != " .. bucketSize)
-    buckets[i]=_.shuffle(bucket)
+    assert(#bucket==bucketSize,#bucket.. " != ".. bucketSize)
+    return bucket
+  end
+
+  local buckets={}
+  for i=1, numBuckets do
+    buckets[i]=_.shuffle(makeBucket())
   end
   return _(buckets):shuffle():flatten():value()
 end
