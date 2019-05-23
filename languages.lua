da12a94cbc4195c6e58e872544846fffdeb31922
Tom Piercy <tap32@medschl.cam.ac.uk>
2019-05-23 12:12:52 +0100

    Converted language files to JSON format

--- languages.lua
+++ languages.lua
@@ -3,19 +3,24 @@ languages = M
 
 local lfs = require ("lfs")
 local i18n = require ("i18n.init")
+local jsonreader = require ("jsonreader")
 local system = system
 local require = require
+local assert = assert
+local print = print
 
 setfenv(1,M)
 
 local path = system.pathForFile("lang",system.ResourceDirectory)
+assert(path, "languages.lua: No path found")
 local langs = {}
 
 for file in lfs.dir(path) do
-	if file:match("lua$") then
-		local langName = file:sub(1,-5)
+	if file:match("json$") then
+		local langName = file:sub(1,-6)
+		print(langName, file)
 		langs[#langs + 1] = langName
-		i18n.load(require ("lang."..langName))
+		i18n.load(jsonreader.load(path.."/"..file))
 	end
 end
 
