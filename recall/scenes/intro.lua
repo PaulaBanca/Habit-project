7a804ad2e870e03444c9051897d04a1dab9ba9de
Tom Piercy <tap32@medschl.cam.ac.uk>
2019-06-11 11:18:59 +0100

    Move existing recall task structure into separate file

--- recall/scenes/intro.lua
+++ recall/scenes/intro.lua
@@ -6,156 +6,15 @@ local events=require "events"
 local _=require "util.moses"
 local serpent=require "serpent"
 local i18n = require ("i18n.init")
+local standardrecall = require ("tasks.standardrecall")
 local display=display
 local type=type
 local math=math
 local timer=timer
-local setmetatable=setmetatable
-local rawget=rawget
+
 local print=print
-local FLAGS=FLAGS
 
 setfenv(1,scene)
-
-local getTrackMT={
-  __index=function(t,k)
-    if k=="track" then
-      return phasemanager.getCurrentTrack()
-    end
-    return rawget(t, k)
-  end
-}
-
-local instructions={
-  {
-    phase='Intro',
-    text=i18n("instructions.welcome"),
-    y=display.contentCenterY-40,
-    onComplete=function()
-      events.fire({type='phase finished'})
-    end
-  },
-  {
-    phase='A',
-    text=i18n("instructions.pattern"),
-    y=5,
-    width=display.contentWidth*7/8,
-    fontSize=15,
-    img=function()
-      local icon=phasemanager.getCurrentStimulus()
-      if icon.contentHeight>display.contentHeight*3/4 then
-        local scale=icon.contentHeight/display.contentHeight*3/4
-        icon:scale(scale,scale)
-      end
-      return icon
-    end,
-  },
-  {
-    phase='A',
-    text=i18n("instructions.phase_a"),
-    y=5,
-    width=display.contentWidth*7/8,
-    fontSize=15,
-    img=function()
-      local icon=phasemanager.getCurrentStimulus()
-      if icon.contentHeight>display.contentHeight*3/4 then
-        local scale=icon.contentHeight/display.contentHeight*3/4
-        icon:scale(scale,scale)
-      end
-      return icon
-    end,
-    scene="scenes.play",
-    params=setmetatable({
-      requireStartButton=true,
-      phase="A1",
-      nextScene="scenes.intro",
-      noSwitch=true,
-      allowRestarts=true,
-      rounds=1,
-      iterations=FLAGS.QUICK_ROUNDS and 1
-    },getTrackMT)
-  },
-  -- PHASE B
-  {
-    phase='B',
-    text=i18n("instructions.phase_b"),
-    y=display.contentCenterY-40,
-    scene="scenes.play",
-    params=setmetatable({
-      requireStartButton=true,
-      phase="B1",
-      nextScene="scenes.intro",
-      noSwitch=true,
-      rounds=1,
-      iterations=FLAGS.QUICK_ROUNDS and 4 or 7
-    },getTrackMT)
-  },
-  {
-    phase='B2',
-    scene="scenes.play",
-    seamless=true,
-    params=setmetatable({
-      requireStartButton=true,
-      phase="B2",
-      nextScene="scenes.intro",
-      noSwitch=true,
-      rounds=1,
-      iterations=FLAGS.QUICK_ROUNDS and 1 or 7
-    },getTrackMT)
-  },
-  -- PHASE C
-  {
-
-    phase='C',
-    text=i18n("instructions.phase_c"),
-    y=5,
-    width=display.contentWidth*7/8,
-    fontSize=10,
-    scene="scenes.play",
-    params=setmetatable({
-      phase="C1",
-      requireStartButton=true,
-      nextScene="scenes.intro",
-      noSwitch=true,
-      rounds=1,
-      iterations=FLAGS.QUICK_ROUNDS and 1 or 15
-    },getTrackMT)
-  },
-  {
-
-    phase='C2',
-    text=i18n("instructions.phase_c2"),
-    y=5,
-    width=display.contentWidth*7/8,
-    fontSize=15,
-    scene="scenes.play",
-    img=function()
-      local icon=phasemanager.getCurrentStimulus()
-      if icon.contentHeight>display.contentHeight*3/4 then
-        local scale=icon.contentHeight/display.contentHeight*3/4
-        icon:scale(scale,scale)
-      end
-      return icon
-    end,
-    params=setmetatable({
-      phase="C2",
-      requireStartButton=true,
-      nextScene="scenes.intro",
-      noSwitch=true,
-      rounds=1,
-      iterations=FLAGS.QUICK_ROUNDS and 1 or 15
-    },getTrackMT)
-  },
-  {
-
-    phase='Outro',
-    text=i18n("instructions.completed"),
-    y=5,
-    width=display.contentWidth*7/8,
-    fontSize=15,
-    noButton=true
-  },
-}
 
 function scene:create()
   events.addEventListener('phase change',function()
@@ -169,7 +28,7 @@ function scene:show(event)
   if event.phase=="did" then
     return
   end
-  local phaseInstructions=_.select(instructions,function(_,v)
+  local phaseInstructions=_.select(standardrecall,function(_,v)
     return v.phase==phasemanager.getPhase()
   end)
   local step=phaseInstructions[self.page or 1]
---
+++ recall/tasks/standardrecall.lua
@@ -0,0 +1,156 @@
+local M = {}
+standardrecall = M
+
+local i18n = require ("i18n.init")
+local phasemanager=require "phasemanager"
+local events=require "events"
+local setmetatable=setmetatable
+local rawget=rawget
+local display = display
+
+local FLAGS=FLAGS
+
+setfenv(1, M)
+
+local getTrackMT={
+  __index=function(t,k)
+    if k=="track" then
+      return phasemanager.getCurrentTrack()
+    end
+    return rawget(t, k)
+  end
+}
+
+return {
+  {
+    phase='Intro',
+    text=i18n("instructions.welcome"),
+    y=display.contentCenterY-40,
+    onComplete=function()
+      events.fire({type='phase finished'})
+    end
+  },
+  {
+    phase='A',
+    text=i18n("instructions.pattern"),
+    y=5,
+    width=display.contentWidth*7/8,
+    fontSize=15,
+    img=function()
+      local icon=phasemanager.getCurrentStimulus()
+      if icon.contentHeight>display.contentHeight*3/4 then
+        local scale=icon.contentHeight/display.contentHeight*3/4
+        icon:scale(scale,scale)
+      end
+      return icon
+    end,
+  },
+  {
+    phase='A',
+    text=i18n("instructions.phase_a"),
+    y=5,
+    width=display.contentWidth*7/8,
+    fontSize=15,
+    img=function()
+      local icon=phasemanager.getCurrentStimulus()
+      if icon.contentHeight>display.contentHeight*3/4 then
+        local scale=icon.contentHeight/display.contentHeight*3/4
+        icon:scale(scale,scale)
+      end
+      return icon
+    end,
+    scene="scenes.play",
+    params=setmetatable({
+      requireStartButton=true,
+      phase="A1",
+      nextScene="scenes.intro",
+      noSwitch=true,
+      allowRestarts=true,
+      rounds=1,
+      iterations=FLAGS.QUICK_ROUNDS and 1
+    },getTrackMT)
+  },
+  -- PHASE B
+  {
+    phase='B',
+    text=i18n("instructions.phase_b"),
+    y=display.contentCenterY-40,
+    scene="scenes.play",
+    params=setmetatable({
+      requireStartButton=true,
+      phase="B1",
+      nextScene="scenes.intro",
+      noSwitch=true,
+      rounds=1,
+      iterations=FLAGS.QUICK_ROUNDS and 4 or 7
+    },getTrackMT)
+  },
+  {
+    phase='B2',
+    scene="scenes.play",
+    seamless=true,
+    params=setmetatable({
+      requireStartButton=true,
+      phase="B2",
+      nextScene="scenes.intro",
+      noSwitch=true,
+      rounds=1,
+      iterations=FLAGS.QUICK_ROUNDS and 1 or 7
+    },getTrackMT)
+  },
+  -- PHASE C
+  {
+
+    phase='C',
+    text=i18n("instructions.phase_c"),
+    y=5,
+    width=display.contentWidth*7/8,
+    fontSize=10,
+    scene="scenes.play",
+    params=setmetatable({
+      phase="C1",
+      requireStartButton=true,
+      nextScene="scenes.intro",
+      noSwitch=true,
+      rounds=1,
+      iterations=FLAGS.QUICK_ROUNDS and 1 or 15
+    },getTrackMT)
+  },
+  {
+
+    phase='C2',
+    text=i18n("instructions.phase_c2"),
+    y=5,
+    width=display.contentWidth*7/8,
+    fontSize=15,
+    scene="scenes.play",
+    img=function()
+      local icon=phasemanager.getCurrentStimulus()
+      if icon.contentHeight>display.contentHeight*3/4 then
+        local scale=icon.contentHeight/display.contentHeight*3/4
+        icon:scale(scale,scale)
+      end
+      return icon
+    end,
+    params=setmetatable({
+      phase="C2",
+      requireStartButton=true,
+      nextScene="scenes.intro",
+      noSwitch=true,
+      rounds=1,
+      iterations=FLAGS.QUICK_ROUNDS and 1 or 15
+    },getTrackMT)
+  },
+  {
+
+    phase='Outro',
+    text=i18n("instructions.completed"),
+    y=5,
+    width=display.contentWidth*7/8,
+    fontSize=15,
+    noButton=true
+  },
+}
+
+
+
