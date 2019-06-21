7c81abf03a0b08d653dd2c3b6faf8440b21b9f5b
Tom Piercy <tap32@medschl.cam.ac.uk>
2019-06-21 11:57:08 +0100

    Ensure the new setup screen works on small screens

--- recall/scenes/setup.lua
+++ recall/scenes/setup.lua
@@ -7,6 +7,7 @@ local i18n = require ("i18n.init")
 local intro = require ("scenes.intro")
 local _ = require ("util.moses")
 local require = require
+local widget = require ("widget")
 
 setfenv(1,scene)
 
@@ -58,13 +59,26 @@ function scene:create()
   local y=20
   local button
   local optionSelected = _.rep(false,#options)
+
+  local scroll = widget.newScrollView({
+    top = 0,
+    left = 0,
+    width = display.contentWidth,
+    height = display.contentHeight,
+    horizontalScrollDisabled = true,
+    verticalScrollDisabled = false,
+    hideBackground = true
+  })
+
+  self.view:insert(scroll)
+
   for i=1,#options do
     local opt=options[i]
     local t=display.newText({
-      parent=self.view,
       text=opt.label,
       fontSize=34
     })
+    scroll:insert(t)
     t.x=display.contentCenterX
     t.y=y
     y=y+t.height
@@ -73,18 +87,24 @@ function scene:create()
     local optWidth=(display.contentWidth*3/4-(20*#opt.options))/#opt.options
     local bgs={}
     for k=1,#opt.options do
-      local bg=display.newRect(self.view,display.contentWidth/8+optWidth/2+(optWidth+20)*(k-1),y,optWidth,50)
+      local bg=display.newRect(
+        display.contentWidth/8+optWidth/2+(optWidth+20)*(k-1),
+        y,
+        optWidth,
+        50)
+      scroll:insert(bg)
       bg:setFillColor(83/255, 148/255, 250/255)
       if k==opt.default then
         bg.strokeWidth=8
       end
       bgs[k]=bg
 
-      display.newText({
-        parent=self.view,
+      local label = display.newText({
         text=opt.options[k],
         fontSize=20
-      }):translate(bg.x, bg.y)
+      })
+      label:translate(bg.x, bg.y)
+      scroll:insert(label)
 
       bg:addEventListener("tap", function()
         for i=1,#bgs do
@@ -101,7 +121,7 @@ function scene:create()
 
   button=display.newGroup()
   button.isVisible=false
-  self.view:insert(button)
+  scroll:insert(button)
   local bg=display.newRect(button,display.contentCenterX,y,display.contentWidth/8,50)
   bg:setFillColor(83/255, 148/255, 250/255)
 
@@ -110,10 +130,11 @@ function scene:create()
     text=i18n("buttons.done"),
     fontSize=20
   }):translate(bg.x, bg.y)
-
   bg:addEventListener("tap", function()
     composer.gotoScene("scenes.intro")
   end)
+
+  scroll:setIsLocked(bg.contentBounds.yMax < display.contentHeight,"vertical")
 end
 scene:addEventListener("create")
 
